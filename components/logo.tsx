67893c696b56124cfd90a07a735b853ae272230a
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-02-26 13:04:26 +0200

    update: update the logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ export function LmscnLogo() {
   return (
     <svg
     width={125}
-    height={80}
+    height={40}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 400 90"
       fill="none"
